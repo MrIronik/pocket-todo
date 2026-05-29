127bb9315578d9e7ccc09a3596618f9a3de6c9d6
Mikolaj Majewski <MrIronik99@gmail.com>
2026-05-29 15:53:06 +0200

    Implemented scanning .c .h and .py files and and saveing it to ~/.pocketTODO/name_project.json

---
+++ internal/scanner/scanner.go
@@ -0,0 +1,111 @@
+package scanner
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"pocket-todo/internal/storage"
+	"strings"
+)
+
+func ScanProject(project_path string) ([]storage.File, error) {
+	all_project_files_paths, err := scanDirForSourceFiles(project_path)
+	if err != nil {
+		fmt.Println("Error - Could not scan project folder path")
+		return nil, err
+	}
+
+	var all_files []storage.File
+
+	for _, file_path := range all_project_files_paths {
+		new_file, err := scanFileForTodos(file_path)
+		if err != nil {
+			return nil, err
+		} else if new_file != nil {
+			all_files = append(all_files, *new_file)
+		}
+	}
+
+	return all_files, nil
+}
+
+func isSourceFile(path string) bool {
+	source_file := map[string]struct{}{
+		".c":  {},
+		".h":  {},
+		".py": {},
+	}
+
+	file_ext := filepath.Ext(path)
+
+	_, isSource := source_file[file_ext]
+	return isSource
+}
+
+func scanDirForSourceFiles(dir_path string) ([]string, error) {
+	var source_files_path []string
+
+	filepath.WalkDir(dir_path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		if isSourceFile(path) {
+			source_files_path = append(source_files_path, path)
+		}
+
+		return nil
+	})
+
+	return source_files_path, nil
+}
+
+func scanFileForTodos(file_path string) (*storage.File, error) {
+	scanned_file := storage.File{
+		Name:            filepath.Base(file_path),
+		Path:            file_path,
+		Number_of_todos: 0,
+	}
+
+	file, err := os.Open(scanned_file.Path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	line_number := 0
+
+	for scanner.Scan() {
+		line_number++
+		line := scanner.Text()
+
+		if strings.Contains(line, "TODO: ") {
+			new_todo := storage.Todo{
+				Line:    line_number,
+				Content: strings.TrimPrefix(line, "TODO: "),
+			}
+
+			scanned_file.Todos = append(scanned_file.Todos, new_todo)
+			scanned_file.Number_of_todos++
+		}
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(scanned_file.Todos) == 0 {
+		return nil, nil
+	}
+
+	return &scanned_file, nil
+}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -8,14 +8,17 @@ import (
 )
 
 type Project struct {
-	Name        string
-	Total_todos int
-	Files       File
+	Name         string
+	Project_path string
+	Total_todos  int
+	Files        []File
 }
 
 type File struct {
-	Name  string
-	Todos []Todo
+	Name            string
+	Path            string
+	Number_of_todos int
+	Todos           []Todo
 }
 
 type Todo struct {
@@ -41,17 +44,16 @@ func Init() error {
 	return nil
 }
 
-func CreateNewProjectFile(project_name string) error {
-	file_name := project_name + "_project.json"
-	new_project_path := filepath.Join(data_folder_path, file_name)
+func CreateNewProjectFile(project_name string, project_path string) error {
+	project_file_path := formatProjectFilePath(project_name)
 
-	_, err := os.Stat(new_project_path)
+	_, err := os.Stat(project_file_path)
 	if err == nil {
 		fmt.Println("Error - Project alredy added!")
 		return err
 	}
 
-	file, err := os.Create(new_project_path)
+	file, err := os.Create(project_file_path)
 	if err != nil {
 		fmt.Println("Error - New project file not created")
 		return err
@@ -61,7 +63,7 @@ func CreateNewProjectFile(project_name string) error {
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
 
-	template := createJsonTemplate(project_name)
+	template := createJsonTemplate(project_name, project_path)
 
 	encoder.Encode(template)
 
@@ -70,10 +72,77 @@ func CreateNewProjectFile(project_name string) error {
 	return nil
 }
 
-func createJsonTemplate(project_name string) *Project {
+func DeleteProjectFile(data Project) error {
+	file_name := data.Name + "_project.json"
+	project_file_path := filepath.Join(data_folder_path, file_name)
+
+	err := os.Remove(project_file_path)
+	if err != nil {
+		fmt.Println("Error - Project file not deleted")
+		return err
+	}
+
+	fmt.Printf("%s deleted\n", data.Name)
+
+	return nil
+}
+
+func SaveProjectFileData(project_name string, new_files []File) error {
+	project := ReadProjectFile(project_name)
+	project_file_path := formatProjectFilePath(project_name)
+
+	_, err := os.Stat(project_file_path)
+	if err != nil {
+		fmt.Println("Error - No Project File Path!")
+		return err
+	}
+
+	project.Total_todos = 0
+	project.Files = new_files
+
+	for _, file := range project.Files {
+		project.Total_todos += file.Number_of_todos
+	}
+
+	file, err := os.Create(project_file_path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	encoder.Encode(project)
+
+	return nil
+}
+
+func ReadProjectFile(project_name string) Project {
+	project_file_path := formatProjectFilePath(project_name)
+
+	file, err := os.Open(project_file_path)
+	if err != nil {
+		fmt.Println("Error - file not open")
+		panic(err)
+	}
+	defer file.Close()
+
+	var project Project
+
+	err = json.NewDecoder(file).Decode(&project)
+	if err != nil {
+		panic(err)
+	}
+
+	return project
+}
+
+func createJsonTemplate(project_name string, project_path string) *Project {
 	return &Project{
-		Name:        project_name,
-		Total_todos: 0,
+		Name:         project_name,
+		Project_path: project_path,
+		Total_todos:  0,
 	}
 }
 
@@ -88,4 +157,11 @@ func formatDataFolderPath() error {
 	data_folder_path = filepath.Join(home_path, data_folder_name)
 
 	return nil
+}
+
+func formatProjectFilePath(project_name string) string {
+	file_name := project_name + "_project.json"
+	project_file_path := filepath.Join(data_folder_path, file_name)
+
+	return project_file_path
 }
